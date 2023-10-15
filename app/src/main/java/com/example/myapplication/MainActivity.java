1fa01d7e16bace162f78262cef09f16a0e050c7b
WhitneyL32 <wnlemon@student.fullsail.edu>
2023-10-14 21:27:00 -0400

    Login Activity & Register Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.myapplication.databinding.ActivityLoginBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -16,7 +9,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
-import com.google.android.material.button.MaterialButton;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,41 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Code added to display the login screen.
-        setContentView(R.layout.activity_login);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        TextView user = (TextView) findViewById(R.id.ed_email);
-        TextView pass = (TextView) findViewById(R.id.ed_password);
 
-        MaterialButton signinbtn = (MaterialButton) findViewById(R.id.sign_in);
+        androidx.appcompat.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
-        //Code for only proceeding if login credentials are correct.
-        signinbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(user.getText().toString().equals("newfront") && pass.getText().toString().equals("newfront")){
-                    Toast.makeText(MainActivity.this, "Login Successful.", Toast.LENGTH_SHORT);
-
-                    binding = ActivityMainBinding.inflate(getLayoutInflater());
-                    setContentView(binding.getRoot());
-
-                    androidx.appcompat.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
-                    setSupportActionBar(myToolbar);
-
-                    BottomNavigationView navView = findViewById(R.id.nav_view);
-                    // Passing each menu ID as a set of Ids because each
-                    // menu should be considered as top level destinations.
-                    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                            .build();
-                    NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_activity_main);
-                    NavigationUI.setupActionBarWithNavController(MainActivity.this, navController, appBarConfiguration);
-                    NavigationUI.setupWithNavController(binding.navView, navController);
-                }
-                else
-                    Toast.makeText(MainActivity.this, "Login Failed.", Toast.LENGTH_SHORT);
-            }
-        });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
 
 
     }
