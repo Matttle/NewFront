3ee154f8a7a6cd58b8720baea58c51d2ad559437
Matthew Lemon <MTLemon@student.fullsail.edu>
2023-10-26 20:06:39 -0400

    Commit for Register switching to LoginActivity when successfully registering an account also, for switching from LoginActivty to home screen when successfully logging in, as well as code for staying logged in.
    Signed-off-by: Matthew Lemon <MTLemon@student.fullsail.edu>

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
+import com.example.myapplication.ui.detention.DetentionActivity;
+import com.google.android.material.button.MaterialButton;
 
 public class DashboardFragment extends Fragment {
 
@@ -24,8 +30,18 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        android.widget.Button menubtn = binding.button2;
+
+        menubtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(new Intent(requireContext(), DetentionActivity.class));
+            }
+        }
+        );
+
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
