3801ced997091754deb440d2b362b394ab53a923
Matthew Lemon <Matthewlemon91@gmail.com>
2023-11-01 06:02:09 -0400

    Added some quality of life and lunch break button functionality.
    Signed-off-by: Matthew Lemon <Matthewlemon91@gmail.com>

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -3,32 +3,29 @@ package com.example.myapplication.ui.dashboard;
 import static com.example.myapplication.ui.auth.LoginActivity.displayName;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import android.content.DialogInterface;
 import androidx.appcompat.app.AlertDialog;
 
-import com.example.myapplication.MainActivity;
-import com.example.myapplication.ui.auth.LoginActivity;
-import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
 import com.example.myapplication.ui.detention.DetentionActivity;
-import com.google.android.material.button.MaterialButton;
 
-import org.w3c.dom.Text;
-
-import java.util.Objects;
+import java.time.chrono.MinguoChronology;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.*;
 
 public class DashboardFragment extends Fragment {
 
@@ -36,8 +33,19 @@ public class DashboardFragment extends Fragment {
     private TextView timerTextView;
     private TextView username;
     boolean timerOn;
+    boolean onLunchBreak;
     Timer timer;
-    TimerTask task;
+    TimerTask secondsTask;
+    TimerTask hoursTask;
+    /*TimerTask lunchTask = new TimerTask() { // Code for lunch timer functionality - Matt
+        public void run() {
+            if (!timerOn)
+                startTimer();
+            else
+                stopTimer();
+        }
+    };*/
+
     public static double time = 0.0;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -48,6 +56,7 @@ public class DashboardFragment extends Fragment {
         View root = binding.getRoot();
 
         ImageView menubtn = binding.imageView2;
+        android.widget.Button lunchBreak = binding.radioButton3;
         android.widget.Button startTimer = binding.radioButton;
         android.widget.Button stopTimer = binding.radioButton2;
         android.widget.Button resetTimer = binding.resetButton;
@@ -69,7 +78,7 @@ public class DashboardFragment extends Fragment {
 
         startTimer.setOnClickListener(new View.OnClickListener() {
             public  void onClick(View view) {
-
+                binding.radioButton.setChecked(false);
                 startTimer();
             }
         }
@@ -77,6 +86,7 @@ public class DashboardFragment extends Fragment {
 
         stopTimer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                binding.radioButton2.setChecked(false);
                 stopTimer();
             }
         }
@@ -84,7 +94,50 @@ public class DashboardFragment extends Fragment {
 
         resetTimer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                binding.resetButton.setChecked(false);
                 resetTimer();
+            }
+        }
+        );
+
+        lunchBreak.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (timerOn) {
+                    binding.radioButton3.setChecked(true);
+                    stopTimer();
+                    onLunchBreak = true;
+                    /*AlertDialog.Builder lunchAlert = new AlertDialog.Builder(requireActivity()); // Code for lunch timer functionality - Matt
+                    lunchAlert.setTitle("Lunch Break");
+                    lunchAlert.setMessage("Enter the time you'll be on lunch. (Minutes)");
+                    final EditText inputText = new EditText(getActivity());
+                    inputText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    inputText.setRawInputType(Configuration.KEYBOARD_12KEY);
+                    lunchAlert.setView(inputText);
+                    lunchAlert.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i){
+                            timer.scheduleAtFixedRate(lunchTask, getMinutesForLunch(inputText.toString()), 10000);
+                        }
+                    }
+                    );
+
+                    lunchAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            binding.radioButton3.setChecked(false);
+                        }
+                    }
+                    );
+                    lunchAlert.show();*/
+                }
+                else {
+                    if (onLunchBreak) {
+                        startTimer();
+                    }
+                    binding.radioButton3.setChecked(false);
+                    /*if (lunchTask != null) // Code for lunch timer functionality - Matt
+                        lunchTask.cancel();*/
+                }
             }
         }
         );
@@ -103,35 +156,37 @@ public class DashboardFragment extends Fragment {
         resetAlert.setPositiveButton("Reset", new DialogInterface.OnClickListener()
         {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
-                if(task != null)
-                {
-                    task.cancel();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(secondsTask != null) {
+                    secondsTask.cancel();
                     time = 0.0;
                     stopTimer();
                     timerTextView.setText(formatTime(0,0,0));
                 }
+                if (hoursTask != null)
+                    hoursTask.cancel();
             }
-        });
+        }
+        );
 
-        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener()
-        {
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
-                //do nothing
+            public void onClick(DialogInterface dialogInterface, int i) {
+
             }
-        });
+        }
+        );
 
         resetAlert.show();
     }
 
     public void startTimer(){
+        binding.radioButton3.setChecked(false);
+        onLunchBreak = false;
         timerTextView = (TextView) binding.TimerClock;
         if (!timerOn) {
             timerOn = true;
-            task = new TimerTask() {
+            secondsTask = new TimerTask() {
                 @Override
                 public void run() {
                     getActivity().runOnUiThread(new Runnable() {
@@ -143,13 +198,26 @@ public class DashboardFragment extends Fragment {
                     });
                 }
             };
-            timer.scheduleAtFixedRate(task, 0, 1000);
+            timer.scheduleAtFixedRate(secondsTask, 0, 1000);
+            hoursTask = new TimerTask() {
+                @Override
+                public void run() {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            binding.textView2.setText("Hours Worked: " + getHours());
+                        }
+                    });
+                }
+            };
+            timer.scheduleAtFixedRate(hoursTask, 0, 36000000);
         }
     }
 
     public void stopTimer(){
         if (timerOn) {
-            task.cancel();
+            secondsTask.cancel();
+            hoursTask.cancel();
             timerOn = false;
         }
     }
@@ -164,15 +232,39 @@ public class DashboardFragment extends Fragment {
         return (String) formatTime(seconds, minutes, hours);
     }
 
+    private String getHours(){
+        int round = (int) Math.round(time);
+
+        int hours = ((round % 86400) / 3600);
+
+        return String.format("%02d",hours);
+    }
+
     private String formatTime(int seconds, int minutes, int hours){
         return String.format("%02d",hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
     }
+
+    /*private long getMinutesForLunch(String string) { // Code for lunch timer functionality - Matt
+        long minToMs = 0;
+        try {
+            minToMs = Long.parseLong(string);
+        } catch(NumberFormatException nfe) {
+            System.out.println("Could not parse " + nfe);
+        }
+        minToMs *= 6000;
+
+        return minToMs;
+    }
+
+    private void startLunch() {
+
+    }*/
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         if (timerOn)
-            task.cancel();
+            secondsTask.cancel();
         binding = null;
     }
 }
