0008d7216df423e2f612f8ab5ebde6a2ef552126
Matthew Lemon <Matthewlemon91@gmail.com>
2023-11-18 17:40:31 -0500

    Dashboard fully functional, Home mostly functional, Profile's important features are functional, and added Payment History page.
    Signed-off-by: Matthew Lemon <Matthewlemon91@gmail.com>

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -56,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
